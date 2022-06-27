ec8fb6e36b6ebb33a10f04d50f03c11bf3a3c901
Амир Нурмухамбетов <thereug6@gmail.com>
2022-06-27 16:04:18 +0600

    Fixed issue with fixed scroll behaviour

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 
 import App from './App';
+import ScrollToTop from './components/scrollToTop';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 
@@ -10,6 +11,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
+      <ScrollToTop />
       <App />  
     </BrowserRouter>
   </React.StrictMode>
