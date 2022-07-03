6567b3660c4b6b490173162afe942bbb61ca7554
Амир Нурмухамбетов <thereug6@gmail.com>
2022-07-03 14:23:55 +0600

    Better Footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,49 +1,99 @@
-import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { faInstagram } from '@fortawesome/free-brands-svg-icons';
 import styles from './Footer.module.css'; 
+
+import { infoData } from '../../schemas/infoData';
 
 function Footer() {
     function SocialLink(props) {
         return (
-            <a href={props.href} className={styles['social-link']}>
-                {props.children}
-            </a>
+            <li className={`${styles['list-item']} ${styles['list-item--circle']}`}>
+                <a href={props.href} className={styles['social-link']}>
+                    {props.children}
+                </a>
+            </li>
         );
     }
 
     function PageLink(props) {
         return (
-            <li className={styles['list-pages-item']}>
-                <Link to={props.to}>
-                    {props.children}
-                </Link>
+            <li className={styles['list-item']}>
+                {
+                    props.to ? (
+                        <a href={props.to} className={props.textStyle}>
+                            {props.children}
+                        </a>
+                    ):(
+                        <span className={props.textStyle}>
+                            {props.children}
+                        </span>
+                    )
+                }
             </li>
+        );
+    }
+
+    function List(props) {
+        return (
+            <div className={styles['list-container']}>
+                <span className='p lrg bold'>{props.title}</span>
+
+                <ul className={`${styles['list']} p`}>
+                    {props.children}
+                </ul>
+            </div>
         );
     }
 
     return (
         <footer>
-            <ul className={styles['list-pages-inline']}>
-                <PageLink to='/'>
-                    <span className='p white'>Home</span>
-                </PageLink>
-                <PageLink to='/news-and-updates'>
-                    <span className='p white'>News & Updates</span>
-                </PageLink>
-                <PageLink to='/registration'>
-                    <span className='p white'>Registration</span>
-                </PageLink>
-                <PageLink to='/committes'>
-                    <span className='p white'>Committes</span>
-                </PageLink>
-            </ul>
-            <span className='p white'>
-                <a href='mailto:halmunstaff@gmail.com' className={styles['email']}>halmunstaff@gmail.com</a>
-            </span>
+            <div className={styles['footer-content']}>
+                <List title='Quick Links'>
+                    <PageLink to='/' textStyle='p'>Home</PageLink>
+                    <PageLink to='../information' textStyle='p'>Information</PageLink>
+                    <PageLink to='../about-us' textStyle='p'>Secretariat</PageLink>
+                    <PageLink to={infoData['registration-link']} textStyle='p'>Registration</PageLink>
+                    <PageLink to='../committes' textStyle='p'>Committes</PageLink>
+                </List>
 
-            <span className={'p gray ' + styles['copyright']}>
-                Website managed by Amir Nurmukhambetov
-            </span>
+                <List title='Contact Us'>
+                    <PageLink to='mailto:halmunstaff@gmail.com'>
+                        <b>Email:</b><br/>
+                        halmunstaff@gmail.com
+                    </PageLink>
+
+                    <PageLink>
+                        <b>Address:</b><br/>
+                        112, Al-Farabi Ave., Almaty
+                    </PageLink>
+
+                    <PageLink>
+                        <b>Phones:</b><br/>
+                        87478303734
+                    </PageLink>
+                </List>
+            </div>
+
+            <div className={styles['container']}>
+
+                <ul className={`${styles['list--inline']} ${styles['list']}`}>
+                    <SocialLink href='https://instagram.com/halmun_official?igshid=YmMyMTA2M2Y='>
+                        <FontAwesomeIcon icon={faInstagram} />
+                    </SocialLink>
+                    <SocialLink href='mailto:halmunstaff@gmail.com'>
+                        <FontAwesomeIcon icon={faEnvelope} />
+                    </SocialLink>
+                    
+                </ul>
+
+                <span className='p ltl'>
+                    Copyright @2022 | Website managed by 
+                    <a href='https://github.com/Hereugo' target='_blank'> Amir Nurmukhambetov</a>
+                </span>
+            </div>
+
         </footer>
     );
 }
