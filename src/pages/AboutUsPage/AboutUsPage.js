1eea0f26166b330a67f309884ad1b50acbcf7ff1
rchu06 <138125934+rchu06@users.noreply.github.com>
2023-07-07 09:45:21 +0600

    Update AboutUsPage.js

--- src/pages/AboutUsPage/AboutUsPage.js
+++ src/pages/AboutUsPage/AboutUsPage.js
@@ -29,7 +29,7 @@ function AboutUsPage(props) {
                     />
                 }
                 desc='Now is the time for youth to find their voice in the world and HALMUN provides just that opportunity. Enhance your public speaking skills, master the power of teamwork and meet people from all over the world.
-                HALMUN 2022 is the second iteration of the conference, this time consisting of 3 committees: the United Nations Security Council (UNSC), the United Nations High Commissioner for Refugees (UNHCR), and the Economic and Financial Committee (ECOFIN).'
+                HALMUN 2023 is the third iteration of the conference, this time consisting of 3 committees: the United Nations Security Council (UNSC), the World Health Organisation (WHO), and the Economic and Financial Committee (ECOFIN).'
             />
             <main>
                 <div className='content content--middle'>
