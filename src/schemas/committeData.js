bec9dd6ae4421b8a135545c093e5e9e5a4c8ce31
rchu06 <138125934+rchu06@users.noreply.github.com>
2023-07-07 10:20:59 +0600

    Update committeData.js

--- src/schemas/committeData.js
+++ src/schemas/committeData.js
@@ -4,35 +4,34 @@ export const committesData = [
         "full-name": "United Nations Security Council",
         "id": "unsc",
         "image": "/images/unsc.jpeg",
-        "short-description": "The United Nations Security Council (UNSC) focuses on peace making and security, as well as the safety of all nations.",
+        "short-description": "The United Nations Security Council (UNSC) focuses on peacemaking and security, as well as the safety of all nations.",
         "detailed-description": "The United Nations Security Council (UNSC) is one of the six principal organs of the United Nations (UN) and is charged with ensuring international peace and security, recommending the admission of new UN members to the General Assembly, and approving any changes to the UN Charter. Its powers include establishing peacekeeping operations, enacting international sanctions, and authorizing military action. The UNSC is the only UN body with the authority to issue binding resolutions on member states.",
         
-        "topic-title": "Resolving the Persian Gulf Crisis",
-        "topic-short-description": "UNSC topic for this year's conference is Resolving the Persian Gulf Crisis.",
-        "topic-description": "In retaliation for Iraq's illegal annexation of Kuwait, an alliance of 35 nations led by the United States launched the Gulf War. On January 17, 1991, an airborne and marine attack started the first fight to drive Iraqi forces out of Kuwait. This bombardment lasted for five weeks. During this time, Iraq started firing projectiles into Israel in an effort to elicit a military reaction from Israel, which the Iraqi authorities hoped would force the coalition's Muslim governments to disband and endanger the coalition against Iraq. Iraq deployed Scud missiles at alliance sites located in Saudi Arabia after its missile assault against Israel failed to elicit the anticipated response.\n\nThe alliance then launched a ground invasion into Kuwait, which was under Iraqi control, on February 24. Coalition troops won the operation handily, liberating Kuwait and immediately starting their drive over the Iraq-Kuwait boundary into Iraq terrain. 100 hours after the ground war started, the alliance stopped advancing and issued a cease-fire.\n\nIraq, Kuwait, and the regions bordering Iraq and Saudi Arabia were the only places where airborne and land combat took place.\n\nAfter the regular broadcasting of footage from cameras onboard American bombers during Operation Desert Storm, the war also acquired the moniker Video Game War. Live newscasts from front lines of the conflict were first introduced during this conflict, primarily by the American network CNN.",
+        "topic-title": "Nuclear Proliferation and Nuclear Strategy",
+        "topic-short-description": "UNSC topic for this year's conference is Nuclear Proliferation and Nuclear Strategy.",
+        "topic-description": "This topic explores the historical background, motivations, and consequences of nuclear proliferation. It examines the reasons why countries seek nuclear weapons, including deterrence, security concerns, prestige, and regional power dynamics. It delves into the challenges posed by nuclear proliferation, such as the impact on global stability, arms races, and the potential for nuclear terrorism..",
 
-        "chair": "iskander-kessikbayev",
-        "vice-chair": "ruihan-chu",
-
-        "background-guideline-link": "/data/BackgroundGuidelines/UNSC.pdf"
+        //"chair": "Amina Meirkhan",
+        
+        //"background-guideline-link": "/data/BackgroundGuidelines/UNSC.pdf"
     },
     {
-        "name": "UNHCR",
-        "full-name": "United Nations High Commissioner for Refugees Committee",
-        "id": "unhcr",
-        "image": "/images/unhcr.jpeg",
-        "short-description": "The United Nations High Commissioner for Refugees is a United Nations agency mandated to aid and protect refugees, forcibly displaced communities, and stateless people, and to assist in their voluntary repatriation, local integration or resettlement to a third country.",
+        "name": "WHO",
+        "full-name": "World Health Organisation",
+        "id": "who",
+        //"image": "/images/unhcr.jpeg",
+        //"short-description": "The United Nations High Commissioner for Refugees is a United Nations agency mandated to aid and protect refugees, forcibly displaced communities, and stateless people, and to assist in their voluntary repatriation, local integration or resettlement to a third country.",
         
         "detailed-description": "The United Nations High Commissioner for Refugees is a United Nations agency mandated to aid and protect refugees, forcibly displaced communities, and stateless people, and to assist in their voluntary repatriation, local integration or resettlement to a third country.",
 
-        "topic-title": "Climate Change and Displacement",
-        "topic-short-description": "UNHCR topic for this year's conference is the Climate Change and Displacement.",
-        "topic-description": "Climate change is the defining crisis of our time and its impacts are disproportionately experienced by people in vulnerable situations, such as those who have fled war and persecution. In 2020 alone, over 30 million displacements were triggered by environmental disasters. Most displaced people remain within the borders of their home country. Research suggests that without proper response measures, climate change could cause more than 200 million people to be internally displaced by 2050.",
+        "topic-title": "Harnessing Research, Information, and Evidence",
+        "topic-short-description": "WHO topic for this year's conference is Harnessing Research, Information, and Evidence.",
+        //"topic-description": "Climate change is the defining crisis of our time and its impacts are disproportionately experienced by people in vulnerable situations, such as those who have fled war and persecution. In 2020 alone, over 30 million displacements were triggered by environmental disasters. Most displaced people remain within the borders of their home country. Research suggests that without proper response measures, climate change could cause more than 200 million people to be internally displaced by 2050.",
 
-        "chair": "selin-chetinelli",
-        "vice-chair": "ulpan-nurdilda",
+        //"chair": "Nuriya Klimenko",
+        //"vice-chair": "Aruzhan Kul-Muhammed",
         
-        "background-guideline-link": "/data/BackgroundGuidelines/UNHCR.pdf"
+        //"background-guideline-link": "/data/BackgroundGuidelines/UNHCR.pdf"
     },
     {
         "name": "ECOFIN",
@@ -42,13 +41,13 @@ export const committesData = [
         "short-description": "The Economic and Financial Affairs Council (ECOFIN) is responsible for EU policy in three main areas: economic policy, taxation  issues and the regulation of financial services.",
         "detailed-description": "The United Nations General Assembly Second Committee, also known as the Economic and Financial Committee is one of the six main committees of the United Nations General Assembly. It deals with global finance and economic matters. The Second Committee meets every year in early October and aims to solve world issues.",
 
-        "topic-title": "Tackling Global Inflation",
-        "topic-short-description": "ECOFIN topic for this year's conference is the Tackling Global Inflation.",
-        "topic-description": "The topic for this year’s ECOFIN is “Tackling Global Inflation”. Before exploring this topic in greater detail, key concepts should be defined and distinctions should be made to avoid any baseline misunderstandings.\n\nInflation is the rate of increase in prices of a basket of goods and services over a given period of time. The most commonly used inflation indexes are the Consumer Price Index (CPI) and the Wholesale Price Index (WPI). The CPI is a measure that examines the weighted average of prices of a basket of goods and services which are of primary consumer needs. The CPI is calculated by taking price changes for each item in the predetermined basket of goods and averaging them based on their relative weight in the whole basket. The prices in consideration are the retail prices of each item, as available for purchase by the individual citizens. The WPI, on the other hand, measures and tracks the changes in the price of goods in the stages before the retail level.",
+        "topic-title": "Developing Effective Measures to Combat Tax Evation and Money Laundering in the Digital Age.",
+        "topic-short-description": "ECOFIN topic for this year's conference is Developing Effective Measures to Combat Tax Evation and Money Laundering in the Digital Age..",
+        //"topic-description": "The topic for this year’s ECOFIN is “Tackling Global Inflation”. Before exploring this topic in greater detail, key concepts should be defined and distinctions should be made to avoid any baseline misunderstandings.\n\nInflation is the rate of increase in prices of a basket of goods and services over a given period of time. The most commonly used inflation indexes are the Consumer Price Index (CPI) and the Wholesale Price Index (WPI). The CPI is a measure that examines the weighted average of prices of a basket of goods and services which are of primary consumer needs. The CPI is calculated by taking price changes for each item in the predetermined basket of goods and averaging them based on their relative weight in the whole basket. The prices in consideration are the retail prices of each item, as available for purchase by the individual citizens. The WPI, on the other hand, measures and tracks the changes in the price of goods in the stages before the retail level.",
 
-        "chair": "alikseniia-martysheva",
-        "vice-chair": "tomiris-kamitova",
+        "chair": "ulpan-nurdilda",
+        //"vice-chair": "",
 
-        "background-guideline-link": "/data/BackgroundGuidelines/ECOFIN.pdf"
+        //"background-guideline-link": "/data/BackgroundGuidelines/ECOFIN.pdf"
     }
 ]
